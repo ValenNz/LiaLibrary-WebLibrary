4238a8a33a5634bbc274b312db46b9cc6657b69e
ValenNz <92833376+ValenNz@users.noreply.github.com>
2022-10-26 22:56:02 +0700

    Add LiaLibrary sourch code

---
+++ user/footer.php
@@ -0,0 +1,51 @@
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    
+    <footer class="footer">
+  	 <div class="container">
+  	 	<div class="row">
+  	 		<div class="footer-col">
+  	 			<h4>company</h4>
+  	 			<ul>
+  	 				<li><a href="#">about us</a></li>
+  	 				<li><a href="#">our services</a></li>
+  	 				<li><a href="#">privacy policy</a></li>
+  	 				<li><a href="#">affiliate program</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>get help</h4>
+  	 			<ul>
+  	 				<li><a href="#">FAQ</a></li>
+  	 				<li><a href="#">shipping</a></li>
+  	 				<li><a href="#">returns</a></li>
+  	 				<li><a href="#">order status</a></li>
+  	 				<li><a href="#">payment options</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>online shop</h4>
+  	 			<ul>
+  	 				<li><a href="#">watch</a></li>
+  	 				<li><a href="#">bag</a></li>
+  	 				<li><a href="#">shoes</a></li>
+  	 				<li><a href="#">dress</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>follow us</h4>
+  	 			<div class="social-links">
+  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i class="fab fa-twitter"></i></a>
+  	 				<a href="#"><i class="fab fa-instagram"></i></a>
+  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
+  	 			</div>
+  	 		</div>
+  	 	</div>
+  	 </div>
+  </footer>
+
+</body>
+</html>
---
+++ user/home.php
@@ -0,0 +1,26 @@
+<?php
+include "header.php";
+?>
+    <h2 align="center">Selamat datang <?=$_SESSION['nama_siswa']?> di website PerpusOnline.</h2>
+<div class="row">
+    <?php 
+    include "koneksi.php";
+    $qry_buku=mysqli_query($conn,"select * from buku");
+    while($dt_buku=mysqli_fetch_array($qry_buku)){
+        ?>
+        <div class="col-md-3 mt-3">
+            <div class="card" >
+            <img src="../foto_buku/<?=$dt_buku['foto']?>" class="card-img-top" width="305px" height="400px">
+              <div class="card-body">
+                <h5 class="card-title"><?=$dt_buku['nama_buku']?></h5>
+                <p class="card-text"><?=substr($dt_buku['deskripsi'], 0,20)?></p>
+              </div>
+            </div>
+        </div>
+        <?php
+    }
+    ?>
+</div>
+<?php
+    include "footer.php";
+?>
